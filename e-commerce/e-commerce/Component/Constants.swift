8411cbfbb8553a7c975f9e47b51f72f847ba4a75
omer cakar <omerrccak@gmail.com>
2025-01-22 16:37:41 +0300

    some settings before start

---
+++ e-commerce/e-commerce/Component/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  e-commerce
+//
+//  Created by ÖMER  on 22.01.2025.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackgound: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
