644a816349185cc2eb2f5834d2d48d4e514782e2
omer cakar <omerrccak@gmail.com>
2025-01-22 15:34:28 +0300

    created project

---
+++ e-commerce/e-commerce/e_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerceApp.swift
+//  e-commerce
+//
+//  Created by ÖMER  on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct e_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
